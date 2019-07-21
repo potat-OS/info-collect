fb70abcde06a55caeebeabe8d640bf2157f70d4f
potat-OS <hanlinjue@gmail.com>
2019-07-21 23:39:40 +0800

    ver 1.3

--- src/main/java/com/yb/config/YbMsg.java
+++ src/main/java/com/yb/config/YbMsg.java
@@ -6,10 +6,10 @@ package com.yb.config;
 public class YbMsg {
     public static final String APP_ID                   = "eb5beab8a6b00c97";
     public static final String APP_SEC                  = "032971f19865865f0e291d1c8f913a7c";
-    public static final String MAIN_PAGE                = "http://119.28.52.142:8080/info-collect/index";
-    public static final String ROOT_URL                 = "http://119.28.52.142:8080/info-collect/";
-    public static final String CALLBACK                 = "http://119.28.52.142:8080/info-collect/auth";
-    public static final String APP_URL                  = "http://119.28.52.142:8080/info-collect/information";
+    public static final String MAIN_PAGE                = "http://118.89.153.182:8080/info-collect/index";
+    public static final String ROOT_URL                 = "http://118.89.153.182:8080/info-collect/";
+    public static final String CALLBACK                 = "http://118.89.153.182:8080/info-collect/auth";
+    public static final String APP_URL                  = "http://118.89.153.182:8080/info-collect/information";
     public static final String YIBAN_OPEN_URL           = "https://openapi.yiban.cn/";
     public static final String YIBAN_USER_VERIFY_ME     = "user/verify_me";
     public static final String YIBAN_USER_TOKEN_INFO    = "user/token_info";
